116a804ec0c8d6ff0b15e241b0b16cec4d1722e8
tuando18 <tuaansdo.18@gmail.com>
2024-05-29 23:46:19 +0700

    Initial commit

---
+++ app/src/main/java/com/dovantuan/lab5_ph31763_kot104/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.dovantuan.lab5_ph31763_kot104
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.dovantuan.lab5_ph31763_kot104.ui.theme.Lab5_PH31763_KOT104Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+//            Bai1()
+//            Bai2()
+            Bai3()
+        }
+    }
+}
